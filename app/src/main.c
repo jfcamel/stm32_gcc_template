4fb075c0c44d5362662f120fb445c68e9b6b79d8
jfcamel <fujita.jun@gmail.com>
2019-05-07 17:47:28 +0900

    Commit leakage

--- app/src/main.c
+++ app/src/main.c
@@ -4,15 +4,16 @@
 
 
 extern void SystemInit(void);
-
+extern void initialise_monitor_handles(void);
 
 void main() {
 
   SystemInit();
+  initialise_monitor_handles();
 
   while(1) {
     // do something
+    printf("hello world\n");
   }
 
-  return 0;
 }
